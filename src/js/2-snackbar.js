824b2bfec5a679928891e667c3f6dbd4d1ae6d90
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2023-12-26 17:14:31 +0100

    update

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,4 +1,35 @@
-// Описаний у документації
-// import iziToast from 'izitoast';
-// Додатковий імпорт стилів
-// import 'izitoast/dist/css/iziToast.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formRes = document.querySelector('.form');
+
+formRes.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = formRes.elements.delay.value;
+  const meaningRad = formRes.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (meaningRad == 'fulfilled') {
+        resolve(`success`);
+      } else {
+        reject(`error`);
+      }
+    }, delay);
+    formRes.reset();
+  });
+  promise
+    .then(value => {
+      iziToast.success({
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${delay} ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        position: 'topRight',
+        message: `❌ Rejected promise in ${delay} ms`,
+      });
+    });
+});
